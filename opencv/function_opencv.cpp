f777b8088ebe3bd8ae85edff9154eab59d9784b4
Isbhorn-Photography <dev@bdaniel.fr>
2015-01-26 12:37:50 +0100

    New realease with a some new features like a function converting Keypoint from OpenCV to ViSP

---
+++ opencv/function_opencv.cpp
@@ -0,0 +1,21 @@
+#include "function_opencv.h"
+
+vector<vpImagePoint> getKeypointsFromOpenCV(string imagePath)
+{
+	// Iniatialisation de l'image en OpenCV
+	cv::Mat image = cv::imread(imagePath);
+	cv::Mat gray;
+	cv::cvtColor(image, gray, CV_BGR2GRAY);
+	cv::vector<cv::KeyPoint> keypoints;
+	// Utilisation de FAST
+	cv::FAST(gray, keypoints, 20);
+
+	// Conversion des Keypoints d'OpenCV vers ViSP
+	vector<vpImagePoint> vpKeypoints;
+	for(size_t i=0; i<keypoints.size(); i++)
+	{
+		vpImagePoint point(keypoints[i].pt.y, keypoints[i].pt.x);	
+		vpKeypoints.push_back(point);
+	}
+	return vpKeypoints;
+}
--- opencv/main.cpp
+++ opencv/main.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <vector>
+#include "function_opencv.h"
+#include <visp/vpImage.h>
+#include <visp/vpImageIo.h>
+#include <visp/vpDisplayX.h>
 
-using namespace cv;
+using namespace std;
 
 int main(int argc, char** argv)
 {
-	std::cout << "Veuillez entrer l'adresse de l'image à traiter: " << std::endl;
+	cout << "Veuillez entrer l'adresse de l'image à traiter: " << endl;
 	string adresse_image;
-	std::cin >> adresse_image;
-	Mat image = imread(adresse_image);
+	cin >> adresse_image;
+	/*Mat image = imread(adresse_image);
 	
 	Mat gray;
 	cvtColor(image, gray, CV_BGR2GRAY);
 	vector<KeyPoint> keypoints;
 	
-	FAST(gray, keypoints, 30);
-	
+	FAST(gray, keypoints, 30);*/
+	vpImage<vpRGBa> img;
+	vpImageIo::read(img, adresse_image);
+	vpDisplayX image(img, 100, 100);
+	vpDisplay::display(img);
+	vpDisplay::flush(img);
+	vpDisplay::getClick(img);
+	vector<vpImagePoint> keypoints = getKeypointsFromOpenCV(adresse_image);
+
 	for(size_t i = 0; i < keypoints.size(); i++)
 	{
-		const KeyPoint &kp = keypoints[i];
-		cv::circle(image, kp.pt, kp.size/2, CV_RGB(255, 0, 0));
+		const vpImagePoint &kp = keypoints[i];
+		vpRGBa red(255,0,0);
+		img[(int)kp.get_i()-1][(int)kp.get_j()]= red;
+		img[(int)kp.get_i()][(int)kp.get_j()-1] = red;
+		img[(int)kp.get_i()][(int)kp.get_j()] = red;
+		img[(int)kp.get_i()][(int)kp.get_j()+1] = red;
+		img[(int)kp.get_i()+1][(int)kp.get_j()] = red;
 	}
 
-	imwrite("Images/resultat.pgm", image);
+	vpDisplayX d(img);
+	vpDisplay::display(img);
+	vpDisplay::flush(img);
+	vpDisplay::getClick(img);
 }
