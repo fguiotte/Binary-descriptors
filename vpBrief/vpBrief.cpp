9fb8f51b944e2954405ca719191ff4fcf57589ac
Karamaz0v1 <florent.guiotte@gmail.com>
2015-02-06 10:52:47 +0100

    Here comes vpBrief !

---
+++ vpBrief/vpBrief.cpp
@@ -0,0 +1,10 @@
+/***************************************************************************
+ *
+ * First BRIEF implementation with Visp
+ *
+ * Author: Florent Guiotte <guiotte.florent@gmail.com>
+ *
+ **************************************************************************/
+
+#include <stdio.h>
+
