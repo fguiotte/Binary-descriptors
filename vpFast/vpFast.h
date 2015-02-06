7d01efe5bab338cd1b16c5252a23ae3d49dc958d
farrelNZL <lebecjohan@gmail.com>
2015-02-06 10:52:38 +0100

    adding vpFast file

--- vpFast/vpFast.h
+++ vpFast/vpFast.h
@@ -1,1 +1,9 @@
- 
+#include <iostream>
+#include <vector>
+#include <visp/vpImage.h>
+#include <visp/vpImageIo.h>
+#include <visp/vpDisplayX.h>
+
+using namespace std;
+
+bool vpFast(vpImage<vpRGBa> img, double i,double j);
--- vpFast/vpGetKeypointFast.cpp
+++ vpFast/vpGetKeypointFast.cpp
@@ -1,17 +1,18 @@
 #include "vpGetKeypointFast.h"
 
-vector<vpImagePoint> getKeypointFromFast(vpImage<vpRGBa> img,int  threshold)
+vector<vpImagePoint> vpGetKeypointFromFast(vpImage<vpRGBa> img,int  threshold)
 {
 	bool kp;
+	int n=0;
 	vector<vpImagePoint> vpKeypoints;
 	// Utilisation de FAST
 	for (int i=0; i<n; i++)
 	{
 		for (int j=0; j<n; j++){
-			kp=vpFast(img, i, j)
+			kp=vpFast(img, i, j);
 			if(kp){
-				vpImagePoint point(img[i],img[j]);
-				pKeypoints.push_back(point);	
+				vpImagePoint point(i,j);
+				vpKeypoints.push_back(point);	
 			}
 		}
 	}
