de212754dab2c0948d660c93a9724c25a28af3c3
farrelNZL <lebecjohan@gmail.com>
2015-03-13 11:17:36 +0100

    again improvement fast

--- vpFast/vpFast.cpp
+++ vpFast/vpFast.cpp
@@ -43,7 +43,42 @@ bool vpFast(vpImage<unsigned char> img, int i,int j, int threshold){
 			if(primaryTestSW>threshold){cpt++;}
 	
 			if(cpt >= 6){
-				return true;
+				int N1,N2,W1,W2,S1,S2,E1,E2;
+
+				N1=img[i-3][j-1];
+				N2=img[i-3][j+1];
+				W1=img[i-1][j+3];
+				W2=img[i+1][j+3];
+				S1=img[i+3][j-1];
+				S2=img[i+3][j+1];
+				E1=img[i-1][j-3];
+				E2=img[i+1][j-3];
+
+				int TestN1 = std::abs(N1-threshold);
+				int TestN2 = std::abs(N2-threshold);
+				int TestW1 = std::abs(W1-threshold);
+				int TestW2 = std::abs(W2-threshold);
+				int TestS1 = std::abs(S1-threshold);
+				int TestS2 = std::abs(S2-threshold);
+				int TestE1 = std::abs(E1-threshold);
+				int TestE2 = std::abs(E2-threshold);
+
+
+				if(TestN1>threshold){cpt++;}
+				if(TestN2>threshold){cpt++;}
+				if(TestW1>threshold){cpt++;}
+				if(TestW2>threshold){cpt++;}
+				if(TestS1>threshold){cpt++;}
+				if(TestS2>threshold){cpt++;}
+				if(TestE1>threshold){cpt++;}
+				if(TestE2>threshold){cpt++;}
+				
+				if(cpt>=12){
+					while (1){
+						int cptIsNeighbor,cptOut;
+					}
+					return true;
+				}
 			}
 		}
 	}
