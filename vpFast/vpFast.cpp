3c4e9ed2cc87a51f6d9472486a15398eba9abaeb
farrelNZL <lebecjohan@gmail.com>
2015-03-13 10:45:10 +0100

    fast improvement

--- vpFast/vpFast.cpp
+++ vpFast/vpFast.cpp
@@ -21,8 +21,30 @@ bool vpFast(vpImage<unsigned char> img, int i,int j, int threshold){
 		int primaryTestE = std::abs(north-threshold);
 		int primaryTestW = std::abs(south-threshold);
 
+		int cpt = 2;
+
 		if((primaryTestE > threshold) || (primaryTestW > threshold)){
-			return true;
+			int NW,NE,SW,SE;
+			if(primaryTestE > threshold){cpt++;}
+			if(primaryTestW > threshold){cpt++;}
+			NW= img[i-2][j-2];
+			NE= img[i-2][j+2];
+			SW= img[i+2][j-2];
+			SE= img[i+2][j+2];
+			
+			int primaryTestNW = std::abs(NW-threshold);
+			int primaryTestNE = std::abs(NE-threshold);
+			int primaryTestSW = std::abs(SW-threshold);
+			int primaryTestSE = std::abs(SE-threshold);
+				
+			if(primaryTestNW>threshold){cpt++;}
+			if(primaryTestSW>threshold){cpt++;}
+			if(primaryTestNE>threshold){cpt++;}
+			if(primaryTestSW>threshold){cpt++;}
+	
+			if(cpt >= 6){
+				return true;
+			}
 		}
 	}
 	
