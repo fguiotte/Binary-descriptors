07ac43645485b2ba94a5da295ad196847e34a5a9
Bibi <brieuc@bdaniel.fr>
2015-05-28 11:11:57 +0200

    suppress GetKeyPointFast.* after a renaming !

--- vpFast/vpGetKeypointFast.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <visp/vpImage.h>
-#include <visp/vpImageIo.h>
-#include <visp/vpDisplayX.h>
-#include "vpFast.h"
-
-vector<vpImagePoint> vpGetKeypointFromFast(vpImage<unsigned char> &img,int threshold);
